193760173f49d330b98e6924ab731e2602bcfa03
Ben Fox <benfox1216@gmail.com>
2020-02-21 09:51:29 -0700

    Complete user story 1

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,6 @@ class Ingredient < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :calories
   
-  has_many :dishes, through: :dish_ingredients
   has_many :dish_ingredients
+  has_many :dishes, through: :dish_ingredients
 end
--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -3,11 +3,22 @@ require 'rails_helper'
 describe "As a visitor" do
   describe "when I visit a dish's show page" do
     before :each do
+      @chef_1 = Chef.create!(name: "Chef Meg")
+      @dish_1 = @chef_1.dishes.create!(name: "Shrimp Alfredo", description: "Fettucini Alfredo w/ shrimp")
       
+      @ingredient_1 = @dish_1.ingredients.create!(name: "Fettucini", calories: 500)
+      @ingredient_2 = @dish_1.ingredients.create!(name: "Alfredo", calories: 600)
+      @ingredient_3 = @dish_1.ingredients.create!(name: "Shrimp", calories: 300)
+      
+      visit "/dishes/#{@dish_1.id}"
     end
     
     it "I see the chef that made the dish and the ingredients" do
-      
+      expect(page).to have_content("#{@dish_1.name}")
+      expect(page).to have_content("#{@chef_1.name}")
+      expect(page).to have_content("#{@ingredient_1.name}")
+      expect(page).to have_content("#{@ingredient_2.name}")
+      expect(page).to have_content("#{@ingredient_3.name}")
     end
   end
 end
