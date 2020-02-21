eb0ffb09c63efc98f3c8eca4edeb355b774ab81e
Ben Fox <benfox1216@gmail.com>
2020-02-21 09:25:43 -0700

    Set up database/relationships for ingredients, begin test for first story

---
+++ spec/features/dishes/show_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "As a visitor" do
+  describe "when I visit a dish's show page" do
+    before :each do
+      
+    end
+    
+    it "I see the chef that made the dish and the ingredients" do
+      
+    end
+  end
+end
